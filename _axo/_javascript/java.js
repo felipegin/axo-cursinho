667152a4be5902002457655c8f84beaae06707bb
Felipe Gin <felipebbgin@gmail.com>
2020-08-21 18:25:18 -0300

    projeto inicial site
    rascunho da primeira ideia para o cursinho.

---
+++ _axo/_javascript/java.js
@@ -0,0 +1,8 @@
+		function ligaseta()
+		{
+			document.getElementById("seta").src = "_imagens/axosite4.png";
+		}
+		function desligaseta()
+		{
+			document.getElementById("seta").src ="_imagens/axosite5.png";
+		}
